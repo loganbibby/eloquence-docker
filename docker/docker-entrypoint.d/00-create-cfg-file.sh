b393c0b7e2b9fe7364d20a9b44282b07bd71c995
Logan Bibby <logan@loganbibby.com>
2025-03-22 13:26:56 -0500

    Update 00-create-cfg-file.sh

--- docker/docker-entrypoint.d/00-create-cfg-file.sh
+++ docker/docker-entrypoint.d/00-create-cfg-file.sh
@@ -2,5 +2,7 @@
 
 if [ ! -s $ELOQ_CFG ]; then
     cp /etc/opt/eloquence/$ELOQ_VERSION/eloqdb.cfg.orig $ELOQ_CFG
+    echo "[db-access]" >> $ELOQ_CFG
+    echo "allow = all" >> $ELOQ_CFG
     exit 0
 fi
